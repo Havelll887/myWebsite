dd2741e9336c1ba57239c8af9e4e715cf23a9377
havelll887 <79336137+Havelll887@users.noreply.github.com>
2023-07-08 16:08:31 +0800

    init project
    init project

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+const country = 'Austria';
+
+export { }; // 👈️ if you don't have anything else to export
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Hello from './views/hello/index'
+
+function App() {
+    return (
+        <Hello />
+    );
+}
+
+export default App;
